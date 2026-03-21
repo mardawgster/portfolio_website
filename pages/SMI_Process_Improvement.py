7039388bc2aa39fd529622903dd4190df39f11ae
mardawgster <mad23900@uga.edu>
2026-03-21 12:25:39 -0400

    added Assembly Line Changes

--- pages/SMI_Process_Improvement.py
+++ pages/SMI_Process_Improvement.py
@@ -18,25 +18,25 @@ col1main1, col2main1 = st.columns([1, 2])
 
 with col1main1:
     st.write("**Tech Stack:**")
-    st.code("CV: YOLOv11\nEdge: ESP32 & M4 Mac Mini\nLogic: Python\nCOM Protocol: MQTT/Mosquitto")
+    st.code("CAD: Fusion 360\n FDM AM: Carbon Fiber Infused Nylon PA12, PETG, PLA\n SLA AM: Rigid 10k Resin")
     # with open("assets/vignette_docs.pdf", "rb") as file:
     #     st.download_button("Download Full Documentation", data=file, mime="application/pdf")
     vig_photo = Image.open("assets/smi_tools.png")
-    st.image(vig_photo, caption="The fully integrated sorting station.")
+    st.image(vig_photo, caption="3D Printed Tab Covers (TOP), Slot & Pin Gauges (MID LEFT), Autoclave Mounts (MID RIGHT), Dibber Tools (BOTTOM) .")
 
 
 
 with col2main1:
     st.markdown("""
-    ### Autonomous Quality Detection for SMEs
-    This project demonstrates a low-cost, robust, internet-independent computer vision system designed for automated agricultural sorting. 
+    ### Process Improvement for Composites Tooling using Additive Manufacturing
+    This project demonstrates the design and development of a custom additive manufacturing system for tooling improvement in composites manufacturing, with a focus on the design, fabrication, and testing of 3D-printed solutions for common challenges in composites tooling manufacturing.
     
     **Key Engineering Achievements:**
-    * **Smoothed Label Algorithm:** Developed a rolling-history classification system to eliminate detection "flickering" and rotation compensation in moving objects.
-    * **Pneumatic Edge-Triggering:** Integrated an IR proximity sensor with "rising edge" logic to ensure millisecond-accurate ejection of defective fruit.
-    * **Accessible Architecture:** Built using an **M4 Mac Mini** and **ESP32**, proving that industrial-grade AI can run on inexpensive, open hardware.
-    * **Open-Source Software:** Runs an open-source YOLO V11 model and trained on real data showing the accessibility of the technology.
-    """)
+    * **Custom Autoclave-Compatible Mounts:** Designed and 3D-printed (FDM) custom mounts for autoclave curing of composite parts, improving the quality and consistency of the curing process while reducing costs and lead times.
+    * **Paint Tabs:** Developed 3D-printed (FDM) paint tabs for composite parts, covering crictial dimensions and features to ensure accurate paint application and reduce the need for manual masking, improving efficiency and quality in the painting process.
+    * **Ergonomic Dibber Tools:** Created custom 3D-printed (FDM) dibber tools for composite layup, designed with ergonomic features to improve operator comfort and reduce fatigue during the layup process, while also improving the precision and consistency of the layup.
+    * **Slot Gauges:** Designed and fabricated 3D-printed (SLA) slot & pin gauges for quality control in composites manufacturing, enabling quick and accurate measurement of critical dimensions and features on composite parts, improving quality assurance.
+                """)
     
     # Demo Video
     st.video("assets/smi_vid.mp4") 
@@ -44,5 +44,5 @@ with col2main1:
 # --- SKILLS & RESEARCH ---
 st.divider()
 st.header("Core Competencies")
-st.write("- **Hardware:** CNC (Genmitsu), Pneumatics (135 psi), PCB Design")
-st.write("- **Software:** Python (OpenCV, Ultralytics), ESPHome, Home Assistant, C++")
+st.write("- **Hardware:** 3D Printing Bambu, Prusa, and Formlabs (FDM & SLA), CAD Design, Composites Manufacturing")
+st.write("- **Software:** Fusion 360, PrusaSlicer, BambuStudio, PreForm")
